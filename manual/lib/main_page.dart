7de7d90cb3a09712eefd1ff85a3f407912c9bf8f
Sylar47 <septemberman@outlook.com>
2019-06-27 17:27:17 +0800

    更新了侧边导航栏的一些细节
    对侧边导航栏drawer添加了userAccountsDrawerHeader用于显示账户，对column外套singlechildscrollview实现expansiontile展开后超出边框问题。

--- manual/lib/main_page.dart
+++ manual/lib/main_page.dart
@@ -17,43 +17,110 @@ class _MainPageState extends State<MainPage> {
           title: Text('手册程序电子化管理系统'),
         ),
         drawer: Drawer(
-          child: Column(
-            children: <Widget>[
-              ExpansionTile(
-                title: Text('item'),
-                trailing: Icon(Icons.arrow_drop_down),
-                children: <Widget>[
-                  ListTile(
-                    title: Text('data'),
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => PageMore()));
-                    },
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountEmail: Text('septemberman@outlook.com'),
+                  accountName: Text('sylar'),
+                  currentAccountPicture: ClipOval(
+                    child: Image.asset('assets/account1.jpg'),
                   ),
-                  ListTile(
-                    title: Text('data'),
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => PageMore()));
-                    },
-                  ),
-                  ListTile(
-                    title: Text('data'),
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => PageMore()));
-                    },
-                  ),
-                ],
-              ),
-              ListTile(
-                title: Text('data'),
-                onTap: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => PageMore()));
-                },
-              ),
-            ],
+                ),
+                ListTile(
+                  title: Text('手册浏览'),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => PageMore()));
+                  },
+                ),
+                ExpansionTile(
+                  title: Text('基本配置'),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('部门配置'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                    ListTile(
+                      title: Text('岗位配置'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                    ListTile(
+                      title: Text('人员配置'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  title: Text('手册录入'),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('手册录入'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                    ListTile(
+                      title: Text('程序录入'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  title: Text('部门配置'),
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('工作名称配置'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                    ListTile(
+                      title: Text('人员工作对接'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                    ListTile(
+                      title: Text('流程工作对接'),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => PageMore()));
+                      },
+                    ),
+                  ],
+                ),
+                ListTile(
+                  title: Text('用户权限配置'),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => PageMore()));
+                  },
+                ),
+                ListTile(
+                  title: Text('手册查询'),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => PageMore()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         body: ListView(
@@ -64,16 +131,21 @@ class _MainPageState extends State<MainPage> {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Container(
-                  child: Text('欢迎使用手册程序电子化管理系统'),
-                  padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 20.0),
+                  child: Text(
+                    '欢迎使用手册程序电子化管理系统',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 20.0),
                 ),
                 Container(
                   child: Image.asset(
                     'assets/login3.jpg',
                     scale: 2.0,
                     fit: BoxFit.cover,
+                    width: 270.0,
                   ),
-                  padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                  padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 20.0),
                 ),
               ],
             ),

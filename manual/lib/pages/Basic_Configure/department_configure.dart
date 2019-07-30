f6a4f305e0723b848cdb8a85aceae3a980fabde5
kreminlin <49581926+kreminlin@users.noreply.github.com>
2019-07-30 18:45:51 +0800

    对手册浏览界面及部门配置更新
    （1）制作手册浏览导航目录；
    （2）部门配置导航目录；

--- manual/lib/pages/Basic_Configure/department_configure.dart
+++ manual/lib/pages/Basic_Configure/department_configure.dart
@@ -10,7 +10,36 @@ class DepartmentConfigure extends StatelessWidget {
       appBar: AppBar(
         title: Text('部门配置'),
       ),
-      body: Text('这里是部门配置界面'),
+      body: DepartmentConfigureDemo(),
     ));
+  }
+}
+
+//部门名称下列表组件
+class DepartmentConfigureDemo extends StatefulWidget {
+  DepartmentConfigureDemo({Key key}) : super(key: key);
+
+  _DepartmentConfigureDemoState createState() => _DepartmentConfigureDemoState();
+}
+
+class _DepartmentConfigureDemoState extends State<DepartmentConfigureDemo> {
+  final List<String> items = new List<String>.generate(10, (i) => "Items $i");
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+         body: new ListView.builder(
+           itemCount: items.length,
+           itemBuilder: (context,index){
+             return new ListTile(
+               leading: new Icon(Icons.account_circle),
+               title: new Text('${items[index]}'),
+               onTap: (){}
+             );
+           },
+         ),
+       ),
+    );
   }
 }

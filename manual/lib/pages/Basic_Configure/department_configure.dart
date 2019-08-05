e491f26b663fc9a425ba5993a041e15153b633f0
kreminlin <49581926+kreminlin@users.noreply.github.com>
2019-08-05 11:27:34 +0800

    Revert "对手册浏览界面及部门配置更新"
    This reverts commit f6a4f305e0723b848cdb8a85aceae3a980fabde5.

--- manual/lib/pages/Basic_Configure/department_configure.dart
+++ manual/lib/pages/Basic_Configure/department_configure.dart
@@ -10,36 +10,7 @@ class DepartmentConfigure extends StatelessWidget {
       appBar: AppBar(
         title: Text('部门配置'),
       ),
-      body: DepartmentConfigureDemo(),
+      body: Text('这里是部门配置界面'),
     ));
-  }
-}
-
-//部门名称下列表组件
-class DepartmentConfigureDemo extends StatefulWidget {
-  DepartmentConfigureDemo({Key key}) : super(key: key);
-
-  _DepartmentConfigureDemoState createState() => _DepartmentConfigureDemoState();
-}
-
-class _DepartmentConfigureDemoState extends State<DepartmentConfigureDemo> {
-  final List<String> items = new List<String>.generate(10, (i) => "Items $i");
- 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-       child: Scaffold(
-         body: new ListView.builder(
-           itemCount: items.length,
-           itemBuilder: (context,index){
-             return new ListTile(
-               leading: new Icon(Icons.account_circle),
-               title: new Text('${items[index]}'),
-               onTap: (){}
-             );
-           },
-         ),
-       ),
-    );
   }
 }

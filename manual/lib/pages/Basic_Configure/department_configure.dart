5937e9ab12be65202b3e3b737f47d65855b060b0
kreminlin <49581926+kreminlin@users.noreply.github.com>
2019-08-05 11:38:08 +0800

    对手册，部门配置目录重置
    （1）对手册目录重置;
    （2）对部门配置重置;

--- manual/lib/pages/Basic_Configure/department_configure.dart
+++ manual/lib/pages/Basic_Configure/department_configure.dart
@@ -14,3 +14,32 @@ class DepartmentConfigure extends StatelessWidget {
     ));
   }
 }
+
+//部门名称下列表组件
+class DepartmentConfigureDemo extends StatefulWidget {
+  DepartmentConfigureDemo({Key key}) : super(key: key);
+
+  _DepartmentConfigureDemoState createState() => _DepartmentConfigureDemoState();
+}
+
+class _DepartmentConfigureDemoState extends State<DepartmentConfigureDemo> {
+  final List<String> items = new List<String>.generate(10, (i) => "Items $i");
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+         body: new ListView.builder(
+           itemCount: items.length,
+           itemBuilder: (context,index){
+             return new ListTile(
+               leading: new Icon(Icons.account_circle),
+               title: new Text('${items[index]}'),
+               onTap: (){}
+             );
+           },
+         ),
+       ),
+    );
+  }
+}
--- manual/lib/pages/handbook_view.dart
+++ manual/lib/pages/handbook_view.dart
@@ -13,3 +13,68 @@ class HandBookView extends StatelessWidget {
     ));
   }
 }
+
+//手册浏览目录
+class ExpansionPanelDemo extends StatefulWidget {
+  ExpansionPanelDemo({Key key}) : super(key: key);
+
+  _ExpansionPanelDemoState createState() => _ExpansionPanelDemoState();
+}
+
+class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
+//定义数组类型;
+  List<int> mList;
+  List<ExpandStateBean> expandStateList;
+  _ExpansionPanelDemoState(){
+    mList = new List();
+    expandStateList = new List();
+    for(int i=0;i<20;i++){
+      mList.add(i);
+      expandStateList.add(ExpandStateBean(i,false));
+    }
+  }
+
+  //申明内部方法;
+  _setCurrentIndex(int index,isExpand){
+    setState(() {
+     expandStateList.forEach((item){
+       if(item.index==index){
+         item.isOpen=!isExpand;
+       }
+     }); 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+         child: ExpansionPanelList(
+           expansionCallback: (index,bol){
+             _setCurrentIndex(index,bol);
+           },
+           children: mList.map((index){
+              return ExpansionPanel(
+                headerBuilder: (context,isExpandded){
+                  return ListTile(
+                    title: Text('This is No.$index'),
+                  );
+                },
+                body: ListTile(
+                  title: Text('expansion no .$index'),
+                  onTap: (){},
+                ),
+                isExpanded: expandStateList[index].isOpen
+              );
+           }).toList(),
+         )
+      ),
+    );
+  }
+}
+
+class ExpandStateBean{
+  var isOpen;
+  var index;
+    ExpandStateBean(this.index,this.isOpen);
+}

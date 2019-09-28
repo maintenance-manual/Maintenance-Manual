19ae4fc8f9cb295d9f093f171276bbaab0148d49
kreminlin <49581926+kreminlin@users.noreply.github.com>
2019-09-28 16:26:38 +0800

    Revert "Merge branch 'master' into kreminlin"
    This reverts commit 2f273d1da17a4b0ad90a9fc3fc689561c60676bf, reversing
    changes made to 5cf24254d80807bdc1de4d8a2d9ecdf50bb3d5cc.

---
+++ manual/lib/login_page.dart
@@ -0,0 +1,147 @@
+import 'package:manual/service/service_method.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'main_page.dart';
+import 'dart:io';
+import 'package:dio/dio.dart';
+
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> loginKey = GlobalKey<FormState>();
+  var _userNameTextFieldController;
+  var _passwordTextFieldController;
+  var userName;
+  var password;
+
+  void login() {
+    var loginForm = loginKey.currentState;
+    if (loginForm.validate()) {
+      loginForm.save();
+      print('userName:' + userName + ' password:' + password);
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => MainPage()));
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    // print('设备高度：${ScreenUtil.screenHeight}');
+    // print('设备宽度：${ScreenUtil.screenWidth}');
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.fromLTRB(60.0, 20.0, 50.0, 20.0),
+              height: ScreenUtil().setHeight(525),
+              width: ScreenUtil().setWidth(1080),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Colors.blueGrey[400],
+                  Colors.blue[600],
+                  Colors.lightBlue[700]
+                ]),
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.elliptical(20, 10)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0)
+                ],
+              ),
+              child: Text(
+                '登录',
+                style: TextStyle(color: Colors.white, fontSize: 50.0),
+              ),
+            ),
+            Container(
+              child: Form(
+                key: loginKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  verticalDirection: VerticalDirection.down,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(60.0, 80.0, 60.0, 5.0),
+                      child: TextFormField(
+                        autofocus: false,
+                        style: TextStyle(color: Colors.black26, fontSize: 18.0),
+                        decoration: InputDecoration(hintText: '请输入用户名'),
+                        textAlign: TextAlign.center,
+                        controller: _userNameTextFieldController,
+                        obscureText: false,
+                        onSaved: (value) {
+                          userName = value;
+                        },
+                        onFieldSubmitted: (value) {},
+                        validator: (value) {
+                          if (value.length == 0) {
+                            return "用户名不能为空";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 55.0),
+                      child: TextFormField(
+                        autofocus: false,
+                        style: TextStyle(color: Colors.black26, fontSize: 18.0),
+                        decoration: InputDecoration(hintText: '请输入密码'),
+                        textAlign: TextAlign.center,
+                        controller: _passwordTextFieldController,
+                        obscureText: true,
+                        onSaved: (value) {
+                          password = value;
+                        },
+                        validator: (value) {
+                          if (value.length == 0) {
+                            return "密码不能为空";
+                          } else if (value.length < 6) {
+                            return "密码长度不够6位";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Container(
+                      height: 45.0,
+                      child: RaisedButton(
+                        elevation: 1.5,
+                        child: Text(
+                          '登录',
+                          style: TextStyle(color: Colors.black, fontSize: 18.0),
+                        ),
+                        color: Colors.white,
+                        padding: EdgeInsets.fromLTRB(35.0, 6.0, 35.0, 6.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        onPressed: login,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      resizeToAvoidBottomPadding: true,
+    );
+  }
+}
--- manual/lib/pages/handbook_view.dart
+++ manual/lib/pages/handbook_view.dart
@@ -1,16 +1,21 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:manual/model/handbookview_model.dart';
+import 'package:manual/provide/handbookview_provide.dart';
+import 'dart:io';
 import 'package:manual/service/service_method.dart';
+import 'package:provide/provide.dart';
 
 class HandBookView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-      appBar: AppBar(
-        title: Text('手册浏览'),
-      ),
-      body: ExpansionPanelDemo()
-    ));
+            appBar: AppBar(
+              title: Text('手册浏览'),
+            ),
+            body: ExpansionPanelDemo()));
   }
 }
 
@@ -23,58 +28,70 @@ class ExpansionPanelDemo extends StatefulWidget {
 
 class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
 //定义数组类型;
-  List<int> mList;
-  List<ExpandStateBean> expandStateList;
-  _ExpansionPanelDemoState(){
-    mList = new List();
-    expandStateList = new List();
-    for(int i=0;i<20;i++){
-      mList.add(i);
-      expandStateList.add(ExpandStateBean(i,false));
-    }
+  // List<ExpandStateBean> expandStateList;
+  // _ExpansionPanelDemoState() {
+  //   mList = new List();
+  //   expandStateList = new List();
+  //   for (int i = 0; i < 20; i++) {
+  //     //mList.add(i);
+  //     expandStateList.add(ExpandStateBean(i, false));
+  //   }
+  // }
+
+  // Future getHttp() async {
+  //   try {
+  //     print('开始获取手册浏览数据.......');
+  //     Dio dio = Dio();
+  //     dio.options.contentType =
+  //         ContentType.parse("application/x-www-form-urlencoded");
+  //     Response response =
+  //         await dio.get("http://47.93.54.102:5000/read/readHandbook");
+  //     return response.data;
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  void _getHandbookView()async{
+    await request('handbookview').then((val){
+          var data = json.decode(val.toString());
+          print(data);
+    });
+  }
+
+
+  @override
+  void initState() {
+    // getHttp().then((val) {
+    //     var data = json.decode(val.toString());
+    //     HandbookViewModel list = HandbookViewModel.formJson(data['manualList']); 
+    //     list.manualList.forEach((item)=>print(item));
+    //   print(val);
+    // });
+    super.initState();
   }
 
   //申明内部方法;
-  _setCurrentIndex(int index,isExpand){
-    setState(() {
-     expandStateList.forEach((item){
-       if(item.index==index){
-         item.isOpen=!isExpand;
-       }
-     }); 
-    });
-  }
+  // _setCurrentIndex(int index, isExpand) {
+  //   setState(() {
+  //     expandStateList.forEach((item) {
+  //       if (item.index == index) {
+  //         item.isOpen = !isExpand;
+  //       }
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SingleChildScrollView(
-         child: ExpansionPanelList(
-           expansionCallback: (index,bol){
-             _setCurrentIndex(index,bol);
-           },
-           children: mList.map((index){
-              return ExpansionPanel(
-                headerBuilder: (context,isExpandded){
-                  return ListTile(
-                    title: Text('输入手册大目录:$index'),
-                  );
-                },
-                body: ListTile(
-                  title: Text('传递手册名称文件$index'),
-                  onTap: (){},
-                ),
-                isExpanded: expandStateList[index].isOpen
-              );
-           }).toList(),
-         )
-      ),
+      child: Text('sssss'),
     );
   }
 }
 
-class ExpandStateBean{
-  var isOpen;
-  var index;
-    ExpandStateBean(this.index,this.isOpen);
-}
+// class ExpandStateBean {
+//   var isOpen;
+//   var index;
+//   ExpandStateBean(this.index, this.isOpen);
+// }

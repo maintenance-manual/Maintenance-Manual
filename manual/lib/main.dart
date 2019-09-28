54765cb01d53a1f3fe063cb2909857c8be184032
kreminlin <49581926+kreminlin@users.noreply.github.com>
2019-09-28 16:22:10 +0800

    Revert "Merge branch 'master' into kreminlin"
    This reverts commit 2f273d1da17a4b0ad90a9fc3fc689561c60676bf, reversing
    changes made to 5cf24254d80807bdc1de4d8a2d9ecdf50bb3d5cc.

--- manual/lib/main.dart
+++ manual/lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
-import 'package:provide/provide.dart';
-import 'provide/handbook_view_list.dart';
 
-void main(){
-  var providers = Providers();
-  var handbookViewListProvide = HandbookViewListProvide();
-
-  providers
-  ..provide(Provider<HandbookViewListProvide>.value(handbookViewListProvide));
-  runApp(ProviderNode(child:MyApp(),providers:providers));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
